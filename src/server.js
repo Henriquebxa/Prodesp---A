7b532dea4892d6b067989b1455de9912f9d819d0
Henriquebxa <Henriquebxa@gmail.com>
2026-05-21 20:52:16 -0300

    modifiquei o style.css para deixar o scroll dentro do registro e do login possiveis, adicionei a versão do express para que funcione com mais eficiencia.

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 3000;
+const port = 3333;
 
 app.use(express.static(path.join(__dirname, '../public')));
 
